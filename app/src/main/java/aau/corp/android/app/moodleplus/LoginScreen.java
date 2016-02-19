ef8f9546a0fe16a88a61a4b646d0db4ef32f2c58
rathiaman <01aman.rathi@gmail.com>
2016-02-19 23:28:42 +0530

    Moodle Plus FrontEnd Version III

--- app/src/main/java/aau/corp/android/app/moodleplus/LoginScreen.java
+++ app/src/main/java/aau/corp/android/app/moodleplus/LoginScreen.java
@@ -1,11 +1,15 @@
 package aau.corp.android.app.moodleplus;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -24,14 +28,62 @@ public class LoginScreen extends AppCompatActivity {
     }
 
 
+    private boolean PressTwice = false;
+    @Override
+    public void onBackPressed(){
+        if(PressTwice){
+
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+            System.exit(0);
+
+        }
+
+        PressTwice = true;
+        Toast.makeText(this, "Press BACK Button again to Exit", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                PressTwice = false;
+            }
+        }, 2000);
+    }
+
+
     public void onButtonClickListener() {
         login_button = (Button) findViewById(R.id.login_button);
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent i = new Intent(LoginScreen.this, HomeScreen.class);
-                startActivity(i);
+                AlertDialog.Builder submit_alert = new AlertDialog.Builder(LoginScreen.this);
+                submit_alert.setMessage("Confirm your details !!!").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                                        /*Intent registration_result_screen = new Intent("com.thenewstark.application5.Registration_result");
+                                        // Intent inte = new Intent(this, Home.class);
+                                        startActivity(registration_result_screen);*/
+
+
+                        Intent i = new Intent(LoginScreen.this, HomeScreen.class);
+                        startActivity(i);
+
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alert = submit_alert.create();
+                alert.setTitle("Alert !!!");
+                alert.show();
+
+
 
             }
         });
