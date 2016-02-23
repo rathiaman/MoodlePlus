d813645212637d8f03ce7e0119660130ac325402
rathiaman <01aman.rathi@gmail.com>
2016-02-23 10:48:04 +0530

    IP Added

---
+++ app/src/main/java/aau/corp/android/app/moodleplus/IPAddress.java
@@ -0,0 +1,17 @@
+package aau.corp.android.app.moodleplus;
+
+/**
+ * Created by Aman Rathi on 22-02-2016.
+ */
+public class IPAddress {
+
+    public static String ipadress = "";
+
+    public static String getName(){
+        return ipadress;
+    }
+
+    public static void setName(String name){
+        ipadress=name;
+    }
+}
