cb908754a4935cb05b2daa0971d7aa61528d4252
Ujjawal Gupta <ujjawalgupta107@gmail.com>
2016-02-24 15:56:02 +0530

    final app 23:2 6 pm ujjawal

---
+++ app/src/main/java/aau/corp/android/app/moodleplus/Name_Class.java
@@ -0,0 +1,17 @@
+package aau.corp.android.app.moodleplus;
+
+/**
+ * Created by Ujjawal Gupta on 24-Feb-16.
+ */
+public class Name_Class {
+
+    public static String name_first_class = "";
+
+    public static String getName(){
+        return name_first_class;
+    }
+
+    public static void setName(String name){
+        name_first_class=name;
+    }
+}
